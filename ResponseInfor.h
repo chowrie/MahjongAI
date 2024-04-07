a4b6338a52e06a1ea69c59a45c4d91ee893def31
chowrie <1520289587@qq.com>
2024-04-07 17:27:58 +0800

    add some special Fan

--- ResponseInfor.h
+++ ResponseInfor.h
@@ -14,3 +14,27 @@ bool canMinGang();
 bool canAnGang();
 
 bool canBuGang();
+
+bool isZimo();
+
+bool isMenQianQing();
+
+bool isDanDiao();
+
+bool isBianZhang();
+
+bool isKanZhang();
+
+bool isHeJueZhang();
+
+bool isBuQiuRen();//不计自摸，不计门前清
+
+bool isQuanQiuRen();//不计单钓
+
+bool isQiangGangHe();//不计和决张
+
+bool isGangShangKaiHua();//不计自摸
+
+bool isSeaMoon();
+
+bool isHandSpring();//不计自摸
